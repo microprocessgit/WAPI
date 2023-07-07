18823239c0e528d902f5bc58916667de28b944bf
microprocessgit <microprocess.teste@gmail.com>
2023-07-07 08:47:15 -0300

    General - added treatment functions

--- src/zap-util.ts
+++ src/zap-util.ts
@@ -1,5 +1,18 @@
-import { find, update, insertMessageFila, getSentMessages } from './services/client';
+import {
+  find,
+  update,
+  insertMessageFila,
+  getMessageActions,
+  getClient,
+  getMessagesUser,
+  deleteMessages,
+  getSentMessages,
+  statusOfSend
+} from './services/client';
 import fs, { mkdir } from 'fs';
+import { get, post } from './services/request';
+import { globSync, glob } from 'glob';
+import Downloader from 'nodejs-file-downloader';
 
 const clientsList = new Map();
 
@@ -18,6 +31,7 @@ export function updateClients(req: any) {
     }
   });
 }
+
 
 function setClientList(clients: any) {
   const { client, key, webhook } = clients;
@@ -59,9 +73,8 @@ export async function uploadMedia(req: any) {
   });
 };
 
-//inverter as barras para linux
 function getUploadPath() {
-  return __dirname + "\\uploads\\";
+  return __dirname + "/uploads/";
 }
 
 //deleta medias da pasta upload
@@ -87,14 +100,180 @@ export async function addMessageToFila(req: any) {
 }
 
 export async function readMessages(req: any) {
-  const myIdsList = req.body.myIds.split(',');
+  let val: any = {}, response: any, myId;
   let data = [];
-  for (const val of myIdsList) {
-    const res = await getSentMessages(val);
-      if(res){
-        data.push({ message: res.message, 
-          timestamp: Number(res.messageTimestamp), id:res.id, myId:res.myId});
+  const res = await getSentMessages(req.body.fromDate);
+  myId = '';
+  for (val of res) {
+    response = await readResponse(val);
+    if (response?.responseMessage == undefined) {
+      data.push(apendMessage(val, null));
+      if (response != undefined) {
+        myId = myId.concat(response, ',');
       }
+    } else {
+      data.push(apendMessage(val, response));
+    }
+  }
+  if (myId != '') {
+    statusOfSend(myId, 'NOTANSWERED');
+  }
+  return removeResponse(data, myId);
+}
+
+function removeResponse(data: any[], myId: any) {
+  const myIds = myId.split(',');
+  for (myId of myIds) {
+    data.filter(res => res.myId == myId)
+      .map(res => {
+        res.response = null;
+      });
+  }
+  for (let i = data.length; i > 0; i--) {
+    if (data[i - 1].statusOfSend === "NOTANSWERED" && !data[i - 1].response) {
+      data.splice(i - 1, 1);
+    }
   }
   return data;
 }
+
+function apendMessage(val: any, response?: any) {
+  return {
+    message: val.message,
+    timestamp: Number(val.messageTimestamp),
+    id: val.id,
+    myId: val.myId,
+    remoteJid: val.remoteJid,
+    response: response == undefined ? null : response,
+    statusOfSend: val.statusOfSend
+  };
+}
+
+async function readResponse(value: any) {
+  let response, res: any = {};
+  if (value.isAction) {
+    res = await getMessageActions(value);
+    for (const val of res) {
+      if (val?.myId != null && value.remoteJid == val?.remoteJid && val.isAction) {
+        response = value.myId + ',' + val.myId;
+        break;
+      } else if (!isNaN(val?.message.conversation)) {
+        response = {
+          responseMessage: val?.message.conversation,
+          timestamp: Number(val?.messageTimestamp)
+        };
+        break;
+      }
+    }
+  }
+  return response;
+}
+
+export async function getMessages(req: any) {
+  let urlImageFromMe, urlImage;
+  const messages = await getMessagesUser(req);
+  for (const val of messages!) {
+    if ((getDecryptMedia(val)) && (!existFile(val.remoteJid + val))) {
+      downloadMedia(val);
+    }
+    if ((val.myId == null) && (!urlImage)) {
+      urlImage = await downloadProfile(val, false);
+    } else if (!urlImageFromMe) {
+      urlImageFromMe = await downloadProfile(val, true);
+    }
+  }
+  return { messages, urlImage, urlImageFromMe };
+}
+
+export async function downloadMedia(value: any) {
+  const res = await post(
+    `${'http://' + process.env.HOST + ':' + process.env.PORT}/${value.sessionId}/messages/download`,
+    { message: value.message }, { "Content-type": "application/json; charset=UTF-8" }, 'arraybuffer');
+  fs.writeFile(getDownloadPath() + getFileName(value), res!.data, err => {
+    if (err)
+      console.log(err)
+  })
+}
+
+async function downloadProfile(value: any, fromMe: any) {
+  let remoteJid
+  if (!fromMe) {
+    remoteJid = value.remoteJid;
+  } else {
+    let res = await getClient(value.sessionId);
+    let telefone = res?.telefone?.replace(/[-+" '()]/g, "");
+    remoteJid = telefone! + process.env.WHATSAPPSERVER;
+  }
+  if (!existFile(remoteJid + '.jpg')) {
+    const res = await get(
+      `${'http://' + process.env.HOST + ':' + process.env.PORT}/${value.sessionId}/contacts/${remoteJid}/photo`,
+      { "Content-type": "application/json; charset=UTF-8" }, 'json');
+    if (res?.data) {
+      const downloader = new Downloader({
+        url: res!.data.url,
+        directory: getDownloadPath(),
+        fileName: remoteJid + '.jpg'
+      });
+      try {
+        await downloader.download();
+      } catch (error) {
+        console.log(error);
+      }
+      return remoteJid + '.jpg';
+    } else {
+      return undefined;
+    }
+  } else {
+    return remoteJid + '.jpg';
+  }
+}
+
+function getDecryptMedia(msg: any) {
+  return msg.message.imageMessage || msg.message.extendedTextMessage?.contextInfo?.quotedMessage.imageMessage ||
+    msg.message.videoMessage || msg.message.extendedTextMessage?.contextInfo?.quotedMessage.videoMessage ||
+    msg.message.audioMessage || msg.message.extendedTextMessage?.contextInfo?.quotedMessage.audioMessage ||
+    msg.message.documentMessage || msg.message.extendedTextMessage?.contextInfo?.quotedMessage.documentMessage;
+}
+
+function getDownloadPath() {
+  let path = require('path').dirname(__dirname);
+  path = path + '/views/chat/downloads/';
+  mkdir(path, { recursive: true }, (err) => { if (err) throw err; });
+  return path;
+}
+
+export function getFileName(value: any) {
+  const decryptMedia = getDecryptMedia(value);
+  if (!decryptMedia) return '';
+
+  const mime = decryptMedia.mimetype;
+  let extension = mime.substring(mime.lastIndexOf("/") + 1).split(";")[0];
+  return value.remoteJid + value.id + '.' + extension;
+}
+
+export async function excluirMensagens(values: any) {
+  //const res = await deleteMessages(values);
+  if (true) {
+    const files = await glob([getDownloadPath() + values.remoteJid + '*']);
+    for (const val of files) {
+      fs.unlink(val, function (err) {
+        if (err) return console.log(err);
+        console.log('file deleted successfully');
+      });
+    }
+  }
+  return true;
+}
+
+function existFile(fileName: any) {
+  const files = globSync(getDownloadPath() + '/**/' + fileName);
+  return files.length > 0;
+}
+
+export async function setStatusOfSend(req: any) {
+  const list = req.body;
+  for (const item of Object.keys(list)) {
+    await statusOfSend(item, list[item])
+  }
+}
+
