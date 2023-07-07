d8086914178ad66e4efb503d8456d178670a0b6b
microprocessgit <microprocess.teste@gmail.com>
2023-07-07 08:40:46 -0300

    Queries - implemented new orm functions

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,4 +1,3 @@
-import { toString } from 'qrcode';
 import { prisma, logger } from '../shared';
 
 export async function insert(client: any) {
@@ -7,15 +6,22 @@ export async function insert(client: any) {
   } catch (e) {
     logger.error(e, 'An error occured during client create');
   }
-
 }
 
 export async function find() {
   return await prisma.client.findMany();
 }
 
+export async function getClient(client: any) {
+  return await prisma.client.findFirst({
+    where: {
+      client: client
+    }
+  });
+}
+
 export async function update(req: any) {
-  var { client, key, webhook, sessionId, sessionKey } = req.body;
+  var { client, key, webhook, telefone, sessionId, sessionKey } = req.body;
   if (client == undefined) client = sessionId;
   if (key == undefined) key = sessionKey;
   try {
@@ -26,7 +32,8 @@ export async function update(req: any) {
       data: {
         client: client,
         key: key,
-        webhook: webhook
+        webhook: webhook,
+        telefone: telefone
       }
     });
   } catch (e) {
@@ -43,7 +50,7 @@ export async function deleteClient(id: any) {
 }
 
 export async function insertMessageFila(req: any) {
-  const { jid, message, options, myId } = req.body;
+  const { jid, message, options, myId, isAction } = req.body;
   const client = req.params.sessionId;
   try {
     await prisma.messageFila.create({
@@ -52,7 +59,8 @@ export async function insertMessageFila(req: any) {
         message: message,
         options: options,
         client: client,
-        myId: myId
+        myId: myId,
+        isAction: isAction
       }
     });
     return true;
@@ -62,18 +70,107 @@ export async function insertMessageFila(req: any) {
   }
 }
 
-export async function getSentMessages(myId: any) {
+export async function getMessageActions(value: any) {
+  const { messageTimestamp, remoteJid } = value;
   try {
     const res = await prisma.message.findMany(
       {
         where: {
-          myId: myId
+          messageTimestamp: { gt: parseInt(messageTimestamp) },
+          remoteJid: remoteJid,
+        },
+        orderBy: {
+          pkId: 'asc'
         }
       }
     );
-    return res[0];
+    return res;
   } catch (e) {
-    logger.error(e, 'An error occurred while querying myId');
+    logger.error(e, 'An error occurred while querying response');
+  }
+}
+
+export async function getSentMessages(dateTime: any) {
+  try {
+    const messageTimestamp = Math.round(new Date(dateTime).getTime() / 1000);
+    const res = await prisma.message.findMany(
+      {
+        where: {
+          messageTimestamp: { gte: messageTimestamp },
+          OR: [
+            {
+              statusOfSend: "NOTSENT"
+            },
+            {
+              statusOfSend: "NOTANSWERED"
+            }
+          ],
+          myId: {
+            not: null
+          }
+        },
+        orderBy: {
+          pkId: 'asc'
+        }
+      }
+    );
+    return res;
+  } catch (e) {
+    logger.error(e, 'An error occurred while querying sent messages');
+    return [];
+  }
+}
+
+export async function getMessagesUser(req: any) {
+  const { remoteJid, timestamp } = req.query;
+  try {
+    return await prisma.message.findMany(
+      {
+        where: {
+          messageTimestamp: { gte: parseInt(timestamp) },
+          remoteJid: {
+            contains: remoteJid
+          }
+        },
+        orderBy: {
+          pkId: 'asc'
+        }
+      }
+    );
+  } catch (e) {
+    logger.error(e, 'An error occurred while querying response');
+  }
+}
+
+export async function deleteMessages(values: any) {
+  const { remoteJid, timestamp } = values;
+  try {
+    const result = await prisma.message.deleteMany(
+      {
+        where: {
+          messageTimestamp: { gte: parseInt(timestamp) },
+          remoteJid: remoteJid
+        }
+      }
+    );
+    return result;
+  } catch (e) {
+    logger.error(e, 'An error occurred while querying response');
     return false;
+  }
+}
+
+export async function statusOfSend(myId: any, status: any) {
+  try {
+      await prisma.message.updateMany({
+        where: {
+          myId: {
+            contains: myId
+          }
+        },
+        data: { statusOfSend: status },
+      });
+  } catch (e) {
+    logger.error(e, 'An error occurred while updating status');
   }
 }
